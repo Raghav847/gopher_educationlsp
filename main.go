488e8807ee2ae62c1a0ed87b44e1d3e3c9ebf25a
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-05-10 15:49:34 -0700

    logger

--- main.go
+++ main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"gopher_educationlsp/rpc"
+	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("hi")
+	logger := getLogger("/Users/raghav/Desktop/gopher_educationlsp/log.txt")
+	logger.Println("Hey, I started")
 
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(rpc.Split)
+
+	for scanner.Scan() {
+		msg := scanner.Text()
+		handleMessage(logger, msg)
+	}
 }
 
-func handleMessage(_ any) {
+func handleMessage(logger *log.Logger, msg any) {
+	logger.Println(msg)
+}
 
+func getLogger(filename string) *log.Logger {
+	logfile, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		panic("bad file name")
+	}
+	return log.New(logfile, "[educationalsp]", log.Ldate|log.Ltime|log.Lshortfile)
 }
